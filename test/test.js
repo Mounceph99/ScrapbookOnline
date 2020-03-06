12182693c7b908fb3461b77c1ad0b7af590d908e
Mounceph99 <Mounceph99@hotmail.com>
2020-03-05 22:12:35 -0500

    Created template for all tests of core features
    template test for:
    register, login, logout, posting, commenting and follwing (plus global feed)

--- test/test.js
+++ test/test.js
@@ -1,9 +1,97 @@
-//const curl = new (require( 'curl-request' ))();
-Main = require('../main.js');
 var assert = require('assert');
 
+// TESTING FILE
+// ALL TESTS REGARDING THE SCRAPBOOK ONLINE WEB APP ARE WRITTEN HERE
+
+//
+// POSTING RELATED TESTS 
+//
 describe('upload', () => {
     it('should not upload', () => {
+
+    });
+});
+
+describe('storage', () => {
+    it('temp', () => {
+
+    });
+});
+
+
+
+describe('/post_image', () => {
+    it('temp', () => {
+
+    });
+});
+
+
+//
+// COMMENTING RELATED TESTS 
+//
+describe('/send_comment', () => {
+    it('temp', () => {
+
+    });
+});
+
+
+describe('/fetch_comment', () => {
+    it('temp', () => {
+
+    });
+});
+
+
+//
+// FOLLOWING RELATED TESTS 
+//
+describe('upload', () => {
+    it('temp', () => {
+
+    });
+});
+
+
+describe('upload', () => {
+    it('temp', () => {
+
+    });
+});
+
+
+//
+// LOGIN RELATED TESTS 
+//
+describe('/login', () => {
+    it('temp', () => {
+
+    });
+});
+
+describe('/logout', () => {
+    it('temp', () => {
+
+    });
+});
+
+//
+// REGISTER RELATED TESTS 
+//
+describe('/register', () => {
+    it('temp', () => {
+
+    });
+});
+
+
+//
+// GLOBAL FEED RELATED TESTS 
+//
+
+describe('/fetch_feed', () => {
+    it('temp', () => {
 
     });
 });
