7c4807a3cb80926fd0f55c63a9acae8005af5f66
Mounceph99 <Mounceph99@hotmail.com>
2020-03-19 21:22:33 -0400

    Implementing unit tests for Posting and Login

--- test/test.js
+++ test/test.js
@@ -35,7 +35,93 @@ it("User is NOT logged in, redirect to login page", function(){
 
 })
 
+it("User should connect to the current session",function(){
+    function nextStub(a){
+        if (a == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        var req = {body : {email : "", password : ""}};
+        var res ={};
+
+        var result = functions.login_feature().loginUser(req,res,nextStub(0));
+
+        assert.equal(result, true);
+
+    };
 });
+
+it("User should NOT connect to the current session",function(){
+    function nextStub(a){
+        if (a == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        var req = {body : {email : "", password : ""}};
+        var res ={};
+
+        var result = functions.login_feature().loginUser(req,res,nextStub(1));
+
+        assert.equal(result, false);
+
+    };
+});
+
+it("Logged in User can logout, destroy session",function(){
+    function nextStub(a){
+            return a;
+    }
+
+    function redirect(){
+        return "/?e=2";
+    }
+
+    function destroy (temp){
+        return 1;
+    }
+
+        var req = {session : {email : "user@gmail.com",destroy}};
+        var res = {redirect};
+
+        var result = functions.login_feature().logoutUser(req,res,nextStub(true));
+
+        assert.equal(result, true);
+
+    
+});
+
+it("Logged in User can logout, but error destroying session", function(){
+    function nextStub(a){
+            return a;
+    }
+
+    function redirect(){
+        return "/?e=2";
+    }
+
+    function destroy (temp){
+        err = true;
+    }
+
+
+        var req = {session : {email : "user@gmail.com",destroy}};
+        var res = {redirect};
+
+        var result = functions.login_feature().logoutUser(req,res,nextStub(false));
+
+        assert.equal(result, "/?e=2");
+
+    
+});
+
+});
+
+
+
 
 describe("Testing register", () => {
     it("User successfully registers", () => {
@@ -78,3 +164,68 @@ describe("Testing Like Feature", function(){
 });
 
 });
+
+describe("Testing Posting", function(){
+    it('Logged in User is not allowed upload files of wrong extension', function(){
+
+        var req = {session:{email:"mounceph99@hotmail.com"}, fileValidationError : "none"};
+        var res = {redirect};
+
+        function redirect(a){
+            return a;
+        }
+
+        var result = functions.posting_feature().postPicture(req,res,null);
+
+        assert.equal(result, "/dashboard?a=2");
+
+    })
+
+    it('Logged in User tried to upload file nothing', function(){
+
+        var req = {session:{email:"mounceph99@hotmail.com"}};
+        var res = {redirect};
+
+        function redirect(a){
+            return a;
+        }
+
+        var result = functions.posting_feature().postPicture(req,res,null);
+
+        assert.equal(result, "/dashboard?a=3");
+
+    })
+
+    it('Logged in User uploaded a valid file', function(){
+
+        var req = {session:{email:"mounceph99@hotmail.com"},  
+                    file : "somefile.png"};
+        var res = {redirect};
+        var flag = "";
+        function redirect(a){
+            flag = a;
+            return a;
+        }
+
+        var result = functions.posting_feature().postPicture(req,res,null);
+
+        assert.equal(flag, "/dashboard?a=1");
+
+    })
+
+    it('Not Logged in User shold be sent back to login page', function(){
+
+        var req = {};
+        var res = {redirect};
+        var flag = "";
+        function redirect(a){
+            flag = a;
+            return a;
+        }
+
+        var result = functions.posting_feature().postPicture(req,res,null);
+
+        assert.equal(flag, "/e=1");
+
+    })
+})
