30137fdc9a1ec0e8dd01c8eef065f324d7dc5c0d
Mounceph Morssaoui <59740037+Mounceph99@users.noreply.github.com>
2020-03-26 12:02:47 -0400

    Implemented tests for index.html

--- test/functions/IndexFunctions.js
+++ test/functions/IndexFunctions.js
@@ -1,48 +1,69 @@
-function login() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText)
-        if (this.responseText == "SUCCESS") {
-          //SHOW AJAX RESPONSE THEN WAIT 5 SECS THEN REDIRECT
-          window.location.href = "/dashboard";
-        } else {
-          document.getElementById("nologin").style.display = "block"
-          document.getElementById("nologin").innerHTML = "Bad User/Pass combination!"
+class Document {
+  constructor() {
+    this.document = "";
+    this.style = {display:""};
+    this.value = {"": this.style,"":"ad0"}
+    
+  }
+    
+  getElementById(arg1){
+    return this.value;
+  }
+}
+var document = new Document();
+
+
+
+function login(testingVariable,xhttp) {
+  xhttp.onreadystatechange = function (testingVariable) {
+
+    if (this.readyState == 4 && this.status == 200) { 
+      if (this.responseText == "SUCCESS") {
+        //SHOW AJAX RESPONSE THEN WAIT 5 SECS THEN REDIRECT
+        testingVariable = "/dashboard";
+        return testingVariable;
+      } else {
+        testingVariable = "block"
+        return testingVariable;
         }
-      }
     }
-    xhttp.open("POST", "/login");
-
-    var email = document.getElementById("email_login").value
-    var password = document.getElementById("password_login").value
-
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(JSON.stringify({ "email": email, "password": password }));
   }
 
-  function register() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText)
-        if (this.responseText == "SUCCESS") {
-          //SHOW AJAX RESPONSE THEN WAIT 5 SECS THEN REDIRECT
-          window.location.href = "/dashboard";
-        } else {
-          document.getElementById("noregister").style.display = "block"
-          document.getElementById("noregister").style.color = "red"
-          document.getElementById("noregister").innerHTML = "Cannot create this user!"
-          //TO DO CHECK RESPONSE THEN CHOOSE WHAT TO WRITE (user already exists or temp failure (db))
-        }
+  xhttp.open("POST", "/login");
+
+  var email = document.getElementById("email_login").value
+  var password = document.getElementById("password_login").value
+
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(JSON.stringify({ "email": email, "password": password }));
+  
+}
+
+function register(testingVariable,xhttp) {
+  
+  xhttp.onreadystatechange = function (testingVariable) {
+    if (this.readyState == 4 && this.status == 200) {
+      if (this.responseText == "SUCCESS") {
+        //SHOW AJAX RESPONSE THEN WAIT 5 SECS THEN REDIRECT
+        return testingVariable = "/dashboard";
+      } else {
+        testingVariable = "block"
+        return testingVariable;
+        //TO DO CHECK RESPONSE THEN CHOOSE WHAT TO WRITE (user already exists or temp failure (db))
       }
     }
-    xhttp.open("POST", "/register");
-
-    var register_email = document.getElementById("register_email").value
-    var register_password = document.getElementById("register_password").value
-    var register_confirm_password = document.getElementById("register_confirm_password").value
-
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(JSON.stringify({ "register_email": register_email, "register_password": register_password, "register_confirm_password" : register_confirm_password }));
   }
+  xhttp.open("POST", "/register");
+
+  var register_email = document.getElementById("register_email").value
+  var register_password = document.getElementById("register_password").value
+  var register_confirm_password = document.getElementById("register_confirm_password").value
+
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(JSON.stringify({ "register_email": register_email, "register_password": register_password, "register_confirm_password": register_confirm_password }));
+}
+
+module.exports = {
+  login,
+  register
+}
--- test/test_index.js
+++ test/test_index.js
@@ -1,22 +1,69 @@
 const assert = require('assert');
 const functions = require('./functions/IndexFunctions');
 
+//Created a stub class for testing
+class XMLHttpRequest {
+    constructor() {
+      this.readyState = 4;
+      this.status = 200;
+      this.responseText = "SUCCESS";
+      this.onreadystatechange;
+    };
+  
+    open(arg1, arg2) {
+  
+    }
+    setRequestHeader(arg1, arg2){
+  
+    }
+    send(a){
+  
+    }
+    setResponseText(arg1){
+        this.responseText = arg1;
+    }
+  }
+
 describe("Testing Functions from index.html", () => {
     describe("Testing login", () => {
-        //var result = functions.login();
-        it("hello", function () {
-            bool = true;
-            assert.equal(true, bool);
+       var xhttp = new XMLHttpRequest();
+       var testingVariable = "unsuccessful test"
+       
+        it("User logged in successfully", function () {
+
+            functions.login(testingVariable, xhttp);
+            var result = xhttp.onreadystatechange(testingVariable);
+            assert.equal(result, "/dashboard");
+        })
+
+        it("User was not able to login", function(){
+            xhttp.setResponseText("FAILED");
+
+            functions.login(testingVariable, xhttp);
+            var result = xhttp.onreadystatechange(testingVariable);
+            assert.equal(result, "block");
         })
     })
 
     describe("Testing register", () => {
-        //var result = functions.register();
-        it("hello", function () {
-            bool = true;
-            assert.equal(true, bool);
-        })
-    })
+        var xhttp = new XMLHttpRequest();
+        var testingVariable = "unsuccessful test"
+        
+         it("User registered in successfully", function () {
+ 
+             functions.register(testingVariable, xhttp);
+             var result = xhttp.onreadystatechange(testingVariable);
+             assert.equal(result, "/dashboard");
+         })
+ 
+         it("User was not able to register", function(){
+             xhttp.setResponseText("FAILED");
+ 
+             functions.register(testingVariable, xhttp);
+             var result = xhttp.onreadystatechange(testingVariable);
+             assert.equal(result, "block");
+         })
+     })
 
 
 })
