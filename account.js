201f46b4a9f533984f53b3b25af64f2c17fe1b0d
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-03-28 11:42:51 -0400

    Create account.js
    encapsulate login and registration logic

---
+++ account.js
@@ -0,0 +1,84 @@
+const md5 = require("md5"); //md5 hashing for password db
+
+function login(router, con) {
+	router.post("/login", function(req, res, next) {
+		const email = req.body.email;
+		const password = req.body.password;
+		//hash md5 and compare to db
+		const hash = md5(password);
+		//connect to db and verify
+		con.query("SELECT * FROM users WHERE email = ? AND password = ?", [email, hash], function(err, result) {
+			if (err) {
+				throw err;
+			}
+			if (result.length == 1) {
+				console.log("User " + email + " has logged in successfully.");
+				//set sessions variable
+				req.session.email = email;
+				req.session.userid = result[0]["uid"];
+				res.send("SUCCESS");
+			} else {
+				res.send("FAIL");
+			}
+		});
+	});
+}
+
+function register(router, con) {
+	router.post("/register", function(req, res, next) {
+		const register_email = req.body.register_email;
+		const register_password = req.body.register_password;
+		const register_confirm_password = req.body.register_confirm_password;
+		//some text with no @'s or spaces followed by an @ then any domain
+		const pattern = /^[^@\s]+@[^\.]+\..+$/g;
+		const valid_password = (password, confirm_password) => {
+			return password.length > 7 && password === confirm_password;
+		};
+		let password_correct = false;
+		let email_correct = false;
+		//1. check same password then check length
+		//2. check email if contain email
+		if (valid_password(register_password, register_confirm_password)) {
+			password_correct = true;
+		}
+		if (pattern.test(register_email)) {
+			//TODO send confirmation email
+			email_correct = true;
+		}
+		const hash = md5(register_password);
+		if (email_correct && password_correct) {
+			con.query("SELECT * FROM users WHERE email = ?", [register_email], function(err, result) {
+				if (err) {
+					throw err;
+				}
+				if (result.length >= 1) {
+					//failsafe
+					console.log("User already exists.");
+					res.end();
+				} else {
+					con.query("INSERT INTO users VALUES (0,?,?,?)", [register_email, hash, "default_avatar.png"], function(
+						err,
+						result
+					) {
+						if (err) {
+							res.send("FAIL");
+							throw err;
+						} else {
+							console.log(
+								"A new user : " + register_email + " has just registered with user id : " + result["insertId"] + "."
+							);
+							req.session.email = register_email;
+							req.session.userid = result["insertId"];
+							res.send("SUCCESS"); //redirect in client browser
+						}
+					});
+				}
+			});
+		} else {
+			res.send("FAIL");
+		}
+	});
+}
+
+exports.login = login;
+exports.register = register;
